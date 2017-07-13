daac69ff5d68a66264fec2ab102c76bd453fd40c
xanaday <dreamwalker.19@gmail.com>
2017-07-12 20:44:39 -0400

    Update country.js

--- country.js
+++ country.js
@@ -1,14 +1,4 @@
-/*
-two different ways to access the country name (though you have to enter the country name to operate it,
-i.e., 'node country.js japan' will log 'japan')
-*/
-const country = process.argv.slice(2).pop();
-const userInput = process.argv[2];
-const fs = require('fs');
-
-const countries = [{name: 'US'}, {name: 'England'}];
-
-fs.readFile('countries.json', function(err, data) { //this is how to parse a JSON file
+fs.readFile('countries.json', function(err, data) {
   var countryInfo = JSON.parse(data);
   countryInfo.forEach(function(obj) {
     if (userInput === obj.name) {
@@ -18,10 +8,3 @@ fs.readFile('countries.json', function(err, data) { //this is how to parse a JSO
     }
   })
 });
-
-// //this is how to iterate through an object's keys and values
-// ObjectKit.forEach = function Object_forEach (object, callback) {
-//   for (var key in object) {
-//     if (object.hasOwnProperty(key)) callback(key, object[key]);
-//   }
-// };
